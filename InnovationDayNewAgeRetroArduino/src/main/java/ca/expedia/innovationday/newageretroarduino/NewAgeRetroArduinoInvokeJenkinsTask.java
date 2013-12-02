65369e55816df5dd41b84f802df41fd53158d170
apires <apires@expedia.com>
2013-12-02 11:44:11 -0500

    Changing some configurations and adding the quartz scheduler to trigger the teeth brusher weapon!

--- InnovationDayNewAgeRetroArduino/src/main/java/ca/expedia/innovationday/newageretroarduino/NewAgeRetroArduinoInvokeJenkinsTask.java
+++ InnovationDayNewAgeRetroArduino/src/main/java/ca/expedia/innovationday/newageretroarduino/NewAgeRetroArduinoInvokeJenkinsTask.java
@@ -1,10 +1,12 @@
 package ca.expedia.innovationday.newageretroarduino;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import ca.expedia.innovationday.newageretroarduino.service.BrushTeethToBlindService;
 
 
+@Component("newAgeRetroArduinoTask")
 public class NewAgeRetroArduinoInvokeJenkinsTask {
  
     @Autowired
--- InnovationDayNewAgeRetroArduino/src/main/java/ca/expedia/innovationday/newageretroarduino/service/NastySound.java
+++ InnovationDayNewAgeRetroArduino/src/main/java/ca/expedia/innovationday/newageretroarduino/service/NastySound.java
@@ -1,6 +1,9 @@
 package ca.expedia.innovationday.newageretroarduino.service;
 
-
+/**
+ * This weapon belongs to the New Age Retro Arduino, and it's used whenever
+ * there is a problem in Jenkins.
+ */
 public interface NastySound {
 
     void on();
