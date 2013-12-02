79828bc7da7cc2f7c260325e4282be85f15744dd
alextimbro <alextimbro@gmail.com>
2013-12-02 13:59:30 -0500

    arduino code

---
+++ InnovationDayNewAgeRetroArduino/src/main/resources/receive.ino
@@ -0,0 +1,22 @@
+int led = 13;
+int buzz = 11;
+int recv = 0;
+
+void setup() {                
+  pinMode(led, OUTPUT);  
+  pinMode(buzz, OUTPUT);  
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {  
+    recv = Serial.read();
+    if (recv == 121) {
+      digitalWrite(led, HIGH);
+      digitalWrite(buzz, HIGH);
+    } else {
+      digitalWrite(led, LOW);
+      digitalWrite(buzz, LOW);
+    }
+  }
+}
